029bff63b0fbefdb55352f163594274596d17bfc
Anton Bernatskiy (alienware) <abernats@uvm.edu>
2014-04-01 10:28:56 -0500

    Cleanpipes script added

---
+++ cleanpipes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+timeout 0.1 cat mlserver/input0
+timeout 0.1 cat mlserver/input1
+timeout 0.1 cat mlserver/input2
+timeout 0.1 cat mlserver/output0
+timeout 0.1 cat mlserver/output1
+timeout 0.1 cat mlserver/output2
