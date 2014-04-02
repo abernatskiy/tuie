6b282ad960b1a7703afe101c7002d60cd4fbd843
Anton Bernatskiy (alienware) <abernats@uvm.edu>
2014-04-01 20:29:32 -0500

    Summary script fixed

--- utils/summary.sh
+++ utils/summary.sh
@@ -7,5 +7,6 @@ SCRDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$SCRDIR"/wintime_calc.sh "$DIR"
 "$SCRDIR"/avg_quality_calc.sh "$DIR"
 "$SCRDIR"/mode_time_calc.sh "$DIR"
+"$SCRDIR"/asym_mode_time_calc.sh "$DIR"
 "$SCRDIR"/success_calc.sh "$DIR"
 
